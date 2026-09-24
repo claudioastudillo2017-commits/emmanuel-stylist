ecd29215785d67ee6ae2569b93e815dcddda0c12
claudioastudillo2017-commits <claudioastudillo2017@gmail.com>
2026-09-23 21:32:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def init_db():
              '', '@emmanuel.stylist', '', '', '', '#c9a86a', '#0a0a0a'))
         business_id = c.lastrowid
 
-        c.execute('INSERT INTO users (business_id, username, password_hash) VALUES (?,?,?)',
+        c.execute('INSERT OR REPLACE INTO business_users(business_id, username, password_hash) VALUES (?,?,?)',
                    (business_id, 'admin', hash_password('emma2026')))
 
         for dow in range(7):
